197c3c2c6ddfd86585728757b214faaf00884bcb
Dxhol <123shoess@gmail.com>
2023-01-07 12:16:09 -0500

    added to js file

--- index.js
+++ index.js
@@ -14,5 +14,38 @@ function data (result, word) {
 		wrapper.classList.add("active");
 		let definitions = result [0].meaning[0].definition[0],
 			phontetics = `${result[0].meaning[0].partOfSpeech} /${result[0].phonetics[0].text}/`;
+		document.querySelector(".word p").innerText = result[0].word;
+		document.querySelector(".word span").innerText = phontetics;
+		document.querySelector(".meaning span").innerText = definitions.definition;
+		document.querySelector(".example span").innerText = definitions.example;
+		audio = new Audio("https:" + result[0].phonetics[0].audio);
+
+		if(definitions.synonyms[0] === undefined){
+			synonyms.parentElement.style.display = "none";
+		} else {
+			synonyms.parentElement.style.display = "block";
+			synonyms.innerHTML = " ";
+			for (let i = 0; i < 5; i++) {
+				let tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[i]},</span>`;
+				tag = i == 4 ? tag = `<span onclick="search('${definitions.synonyms[i]}">${definitions.synonyms[4]}</span>` : tag;
+				synonyms.insertAdjacentHTML("beforeend", tag);
+			}
+		}
 	}
 }
+
+function search(word) {
+	fetchApi(word);
+	searchInput.value = word;
+}
+
+function fetchApi(word){
+	wrapper.classList.remove("active");
+	infoText.style.color = "#000";
+	infoText.innerHTML = `Searching the meaning of <span>"$word"</span>`;
+	let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+	fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() => {
+		infoText.innerHTML = `cannot find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+	});
+}
+
